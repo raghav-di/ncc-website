f7225cb00d09ee356b9076564f0a3c6c931613a1
raghavd7 <raghavdhyani15@gmail.com>
2024-12-28 11:52:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './components/navbar/navbar.jsx';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
